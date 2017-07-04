8cab63c4538155c7dd7d959a3d14b4376d3b78da
mohamadrabee <mohamad_rabee10@yahoo.com>
2017-07-04 06:02:13 +0300

    add updateFriendsCharts and newFriendAdded

--- index.js
+++ index.js
@@ -11,17 +11,21 @@ exports.updateFirebaseUidInFriendsList = functions.database.ref('/users/{userUid
                                              const index = event.params.index
                                              const userUid = event.params.userUid
                                              const friendFacebookUid = event.data.val()
-                                             console.log("facbookIud",friendFacebookUid)
-                                             admin.database().ref(`/facebookUidVsFirebaseUid/${friendFacebookUid}`).once('value')
+                                             return admin.database().ref(`/facebookUidVsFirebaseUid/${friendFacebookUid}`).once('value')
                                              .then(firebaseUid => {
-                                                 console.log("firebaseUid.val()",firebaseUid.val())
                                                  admin.database().ref(`users/${userUid}/friendsList/${index}/firebaseUid`).set(firebaseUid.val())
+                                                 // update friends chart
+                                                .then(res => {
+                                                    admin.database().ref(`users/${firebaseUid.val()}/userCharts`).once('value')
+                                                    .then(friendCharts => {
+                                                        admin.database().ref(`users/${userUid}/friendsCharts`).update(friendCharts.val())
+                                                    })
+                                                })
                                                  admin.database().ref(`users/${firebaseUid.val()}/friendsList`).once('value')
                                                  .then(friends => {
                                                      friends.forEach(friend => {
                                                          admin.database().ref(`users/${userUid}/userInfo/facebookUid`).once('value')
                                                          .then(userFacebookUid => {
-                                                             console.log(userFacebookUid.val(),friend.val().facebookUid)
                                                              if(userFacebookUid.val() === friend.val().facebookUid){
                                                                  admin.database().ref(`users/${firebaseUid.val()}/friendsList/${friend.key}/firebaseUid`)
                                                                  .set(userUid)
@@ -31,4 +35,34 @@ exports.updateFirebaseUidInFriendsList = functions.database.ref('/users/{userUid
                                                  })
                                              })
                                          })
+// update friendsCharts when user add chart
+exports.updateFriendsCharts = functions.database.ref(`/users/{userUid}/userCharts/{chartKey}/{createdAt}`)
+                              .onWrite(event => {
+                                    const userUid = event.params.userUid;
+                                    const chartKey = event.params.chartKey;
+                                    const createdAt = event.params.createdAt;
+                                    const isExist = event.data.val();
+                                    let userFriends =[];
+                                    return admin.database().ref(`users/${userUid}/friendsList`).once('value')
+                                    .then(friends => {
+                                        friends.forEach(friend => {
+                                            userFriends.push(friend.val().firebaseUid);
+                                        })
+                                        for(let f of userFriends){
+                                            admin.database().ref(`/users/${userUid}/userCharts/${chartKey}`).once('value')
+                                            .then(chartToAdd => {
+                                                admin.database().ref(`users/${f}/friendsCharts/${chartKey}`).set(chartToAdd.val())
+                                            })
+                                        }
+                                    })
+                              })
+exports.newFriendAdded = functions.database.ref('/users/{userUid}/friendsList/{index}/firebaseUid')
+                         .onWrite(event => {
+                             const userUid = event.params.userUid;
+                             const friendFireUid = event.data.val()
+                             return admin.database().ref(`/users/${friendFireUid}/userCharts`).once('value')
+                             .then(friendCharts => {
+                                 admin.database().ref(`users/${userUid}/friendsCharts`).update(friendCharts.val())
+                             })
+                         })
 
